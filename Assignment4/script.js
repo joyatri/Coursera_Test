ed953f89f503db4cf7e4c838c78c5349df8ef69a
joyatri <93981987+joyatri@users.noreply.github.com>
2022-02-23 14:20:08 +0530

    Update script.js

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -1,18 +1,11 @@
-
-(function () {
-
-var names = ["joyatri", "patel", "hiral", "sharda", "Pratik", "Frenny", "Lata", "Puja", "Lila", "Jimmy"];
-
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
-  }
-  else {
-    helloSpeaker(names[i]);
-  }
-}
-
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
 })();
-
